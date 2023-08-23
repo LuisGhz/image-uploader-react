71392e95a0a87e40c4d5ceec791d33644e808152
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-23 17:58:59 -0600

    Fix Title

--- src/components/Uploading.tsx
+++ src/components/Uploading.tsx
@@ -5,6 +5,7 @@ export const Uploading = () => {
   return (
     <div className="loading">
       <DarkLightMode />
+      <h1 className="loading__title">Uploading...</h1>
       <div className="loading__bar-container">
         <div className="loading__bar"></div>
       </div>
