12f70486dafdbf3d3aee911f7de87f28d83fbd37
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-23 17:48:45 -0600

    Implement dark mode var colors

--- src/components/Uploading.tsx
+++ src/components/Uploading.tsx
@@ -1,8 +1,10 @@
+import { DarkLightMode } from "./DarkLightMode";
 import "./Uploading.css";
 
 export const Uploading = () => {
   return (
     <div className="loading">
+      <DarkLightMode />
       <div className="loading__bar-container">
         <div className="loading__bar"></div>
       </div>
